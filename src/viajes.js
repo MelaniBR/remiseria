d7d1a382f8c90d2d6643c94710c87e7b38091ad1
MelaniBR <melaniromero1999@hotmail.com>
2020-07-20 12:26:52 -0300

    cambios

--- src/viajes.js
+++ src/viajes.js
@@ -1,15 +1,21 @@
-class Viaje {
+export class Viaje {
     kilometros
     tiempoMaximo
     cantPasajeros
     coloresInconpatibles
+    _vehiculo
     constructor(km, tiempoMaximo, cantPasajeros, coloresInconpatibles) {
         this.kilometros = km
         this.tiempoMaximo = tiempoMaximo
         this.cantPasajeros = cantPasajeros
         this.coloresInconpatibles = coloresInconpatibles
     }
-
+    asignarVehiculo(vehiculo){
+      this._vehiculo = vehiculo
+    }
+    vehiculo(){
+      return this._vehiculo
+    }
     velocidadPromedio() {
         return this.kilometros / this.tiempoMaximo
     }
@@ -27,5 +33,8 @@ class Viaje {
     }
     tieneCapacidad(vehiculo){
         return cantPasajeros >=  vehiculo.capacidad()
+    }
+    lugaresLibres(){
+      return this._vehiculo.capacidad() - this.cantPasajeros 
     }
 }
