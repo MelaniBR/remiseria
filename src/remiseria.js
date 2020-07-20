fdd5f77f9ed4957509a308cfc383228850b775b7
MelaniBR <melaniromero1999@hotmail.com>
2020-07-20 15:37:24 -0300

    cambios

--- src/remiseria.js
+++ src/remiseria.js
@@ -1,92 +1,95 @@
 export class Remiseria {
-    vehiculos = []
-    viajes = []
-    minimoDeViaje = 30
-    
-    agregarFlota(vehiculo) {
-        this.vehiculos.push(vehiculo)
-    }
-    quitarDeFlota(vehiculo) {
-        var i = this.vehiculos.indexOf(vehiculo);
-       this.vehiculos.splice(i, vehiculo);
-    }
-    esRecomendable() {
-        return this.tieneAlMenos3()  &&this.puedenIrA100()
-    }
-    tieneAlMenos3() {
-        return this.cantVehiculos() >= 3
-    }
-    puedenIrA100() {
-      return  this.vehiculos.every(vehiculo => vehiculo.velocidadMaxima() >= 100)
-    }
-    capacidadTotalYendoA(velocidad) {
-        capacidadTotal = 0
-        lista = this.velocidadMaximaMayorOIgual(velocidad)
-        lista.forEach(vehiculo => { capacidadTotal = + vehiculo.capacidad() });
-        return capacidadTotal
+  vehiculos = []
+  viajes = []
+  minimoDeViaje = 30
 
-    }
-    velocidadMaximaMayorOIgual(velocidad) {
-        return this.vehiculos.filter(vehiculo => vehiculo.velocidadMaxima() >= velocidad)
+  agregarFlota(vehiculo) {
+    this.vehiculos.push(vehiculo)
+  }
+  
+  quitarDeFlota(vehiculo) {
+    var i = this.vehiculos.indexOf(vehiculo)
+    this.vehiculos.splice(i, vehiculo)
+  }
+  
+  esRecomendable() {
+    return this.tieneAlMenos3() && this.puedenIrA100()
+  }
+  
+  tieneAlMenos3() {
+    return this.cantVehiculos() >= 3
+  }
+  
+  puedenIrA100() {
+    return this.vehiculos.every(vehiculo => vehiculo.velocidadMaxima() >= 100)
+  }
+  
+  capacidadTotalYendoA(velocidad) {
+    var capacidadTotal = 0
+    var lista = this.velocidadMaximaMayorOIgual(velocidad)
+    lista.forEach(vehiculo => capacidadTotal += vehiculo.capacidad())
+    return capacidadTotal
+  }
+  
+  velocidadMaximaMayorOIgual(velocidad) {
+    return this.vehiculos.filter(vehiculo => vehiculo.velocidadMaxima() >= velocidad)
+  }
+  
+  colorDelAutoMasRapido() {
+    const autoMasRapido = this.vehiculos.reduce((autoPrevio, autoSiguiente) => (autoPrevio.velocidadMaxima() > autoSiguiente.velocidadMaxima()) ? autoPrevio : autoSiguiente)
+    return autoMasRapido.color()
 
-    }
-    colorDelAutoMasRapido() {
-        var maxValue = this.vehiculos[0]
-        for (var i = 1; i < myArray.length; i++) {
-            var currentValue = myArray[i];
-            if (currentValue.velocidadMaxima() > maxValue.velocidadMaxima()) {
-                maxValue = currentValue;
-            }
-        }
-    }
-    
-    cantVehiculos(){
-      return this.vehiculos.length
-    }
-    puedeHacerElViaje(viaje,vehiculo){
-      return viaje.puedeHacerElViaje(vehiculo)
-    }
-	
+  }
+
+  cantVehiculos() {
+    return this.vehiculos.length
+  }
+  
+  puedeHacerElViaje(viaje, vehiculo) {
+    return viaje.puedeHacerElViaje(vehiculo)
+  }
+
   cantDeViajesHechosConMas(kilometros) {
-		return  this.viajesHechosConMasDe(kilometros).length
-	}
-	
-	 viajesHechosConMas(km) {
-		return this.viajes.filter(viaje => viaje.kilometros() > km) 
-	}
-  precioPorViaje(viaje){
+    return this.viajesHechosConMasDe(kilometros).length
+  }
+
+  viajesHechosConMas(km) {
+    return this.viajes.filter(viaje => viaje.kilometros() > km)
+  }
+  
+  precioPorViaje(viaje) {
     return Math.max(this.valorPorKilometro(viaje), this.minimoDeViaje)
   }
 
-	 pagaTotalA(vehiculo) {
-		return this.precioTotalDeViajes(this.viajesHechosCon(vehiculo))
-	}
-	
-	 precioTotalDeViajes(viajes) {
-     total = 0 
-     viajes.forEach(viaje => total =+ this.precioPorViaje(viaje))
-		return total
-	}
-	
-	cantViajes(vehiculo){
-     return this.viajesHechosCon(vehiculo).length
-    }
-    viajesHechosCon(vehiculo){
-      return viajes.filter(viaje => viaje.vehiculo() == vehiculo)
-    }
+  pagaTotalA(vehiculo) {
+    return this.precioTotalDeViajes(this.viajesHechosCon(vehiculo))
+  }
 
-	
+  precioTotalDeViajes(viajes) {
+    total = 0
+    viajes.forEach(viaje => total +=  this.precioPorViaje(viaje))
+    return total
+  }
+
+  cantViajes(vehiculo) {
+    return this.viajesHechosCon(vehiculo).length
+  }
+  
+  viajesHechosCon(vehiculo) {
+    return viajes.filter(viaje => viaje.vehiculo() == vehiculo)
+  }
+
   valorPorKilometros(viaje) {
-		return  this.valorPorKm * viaje.kilometros() 
-	}
-  registrarViaje(viaje,vehiculo){
-      viaje.asignarVehiculo(vehiculo)
-      viajes.push(viaje)
-    }
-    
-    
-    lugaresLibres(){
-      var lugaresLibresTotal = 0
-      viajes.forEach(viaje => lugaresLibresTotal += viaje.lugaresLibres() )
-    }
+    return this.valorPorKm * viaje.kilometros()
+  }
+  
+  registrarViaje(viaje, vehiculo) {
+    viaje.asignarVehiculo(vehiculo)
+    viajes.push(viaje)
+  }
+
+  lugaresLibres() {
+    var lugaresLibresTotal = 0
+    viajes.forEach(viaje => lugaresLibresTotal += viaje.lugaresLibres())
+  }
 }
